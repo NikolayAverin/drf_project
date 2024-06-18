ef2dfcbe742fe813a5e697b8149b806c24a3e2fd
Nikolay Averin <rizzzon116@mail.ru>
2024-06-18 21:12:13 +0400

    install drf, create db, users, course and lesson

---
+++ materials/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(
+        max_length=150,
+        verbose_name="Название курса",
+        help_text="Введите название курса",
+    )
+    description = models.TextField(
+        verbose_name="Описание курса", help_text="Введите описание курса"
+    )
+    preview = models.ImageField(
+        upload_to="course",
+        blank=True,
+        null=True,
+        verbose_name="Превью",
+        help_text="Загрузите превью для курса",
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    title = models.CharField(
+        max_length=150,
+        verbose_name="Название урока",
+        help_text="Введите название урока",
+    )
+    description = models.TextField(
+        verbose_name="Описание урока", help_text="Введите описание урока"
+    )
+    preview = models.ImageField(
+        upload_to="lesson",
+        blank=True,
+        null=True,
+        verbose_name="Превью",
+        help_text="Загрузите превью для курса",
+    )
+    video = models.URLField(
+        verbose_name="Видеоурок", help_text="Введите ссылку на видео с уроком"
+    )
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, verbose_name="Курс", help_text="Выберите курс"
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
