4687bc106dcf6d805d1126d25ab497584abd7d13
Nikolay Averin <rizzzon116@mail.ru>
2024-06-24 20:43:20 +0400

    add authorization

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users.apps import UsersConfig
-from users.views import PaymentListAPIView, UserViewSet
+from users.views import PaymentListAPIView, UserCreateApiView
 
 app_name = UsersConfig.name
 
-router = SimpleRouter()
-router.register("", UserViewSet)
-
 urlpatterns = [
-    path("payments/", PaymentListAPIView.as_view(), name="payments")
-] + router.urls
+    path("payments/", PaymentListAPIView.as_view(), name="payments"),
+    path('token/', TokenObtainPairView.as_view(permission_classes=(AllowAny, )), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny, )), name='token_refresh'),
+    path('register/', UserCreateApiView.as_view(), name='register')
+]
